1ce5f5c3df3ee76292a6e7dba4e4072abe1b85f3
JDereje03 <joseph.dereje@hotmail.com>
2025-12-01 07:26:12 -0500

    Add files via upload

---
+++ interrupts_EP_<101275351>_<101316700>.cpp
@@ -0,0 +1,175 @@
+/**
+ * @file interrupts.cpp
+ * @brief template main.cpp file for Assignment 3 Part 1 of SYSC4001
+ * 
+ */
+
+#include<interrupts_student1_student2.hpp>
+
+void FCFS(std::vector<PCB> &ready_queue) {
+    std::sort( 
+                ready_queue.begin(),
+                ready_queue.end(),
+                []( const PCB &first, const PCB &second ){
+                    return (first.arrival_time > second.arrival_time); 
+                } 
+            );
+}
+
+std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std::vector<PCB> list_processes) {
+
+    std::vector<PCB> ready_queue;   //The ready queue of processes
+    std::vector<PCB> wait_queue;    //The wait queue of processes
+    std::vector<PCB> job_list;      //A list to keep track of all the processes. This is similar
+                                    //to the "Process, Arrival time, Burst time" table that you
+                                    //see in questions. You don't need to use it, I put it here
+                                    //to make the code easier :).
+
+    unsigned int current_time = 0;
+    PCB running;
+
+    //Initialize an empty running process
+    idle_CPU(running);
+
+    std::string execution_status;
+
+    //make the output table (the header row)
+    execution_status = print_exec_header();
+
+    //Loop while till there are no ready or waiting processes.
+    //This is the main reason I have job_list, you don't have to use it.
+    while(!all_process_terminated(job_list) || job_list.empty()) {
+
+        //Inside this loop, there are three things you must do:
+        // 1) Populate the ready queue with processes as they arrive
+        // 2) Manage the wait queue
+        // 3) Schedule processes from the ready queue
+
+        //Population of ready queue is given to you as an example.
+        //Go through the list of proceeses
+        for(auto &process : list_processes) {
+            if(process.arrival_time == current_time) {//check if the AT = current time
+                //if so, assign memory and put the process into the ready queue
+                assign_memory(process);
+
+                process.state = READY;  //Set the process state to READY
+                ready_queue.push_back(process); //Add the process to the ready queue
+                job_list.push_back(process); //Add it to the list of processes
+
+                execution_status += print_exec_status(current_time, process.PID, NEW, READY);
+            }
+        }
+
+        ///////////////////////MANAGE WAIT QUEUE/////////////////////////
+        static unsigned int last_io_at_cpu_time = 0;
+
+        
+        std::vector<PCB> still_waiting;
+        for(auto &process : wait_queue) {
+            if(process.start_time != -1 && current_time >= (unsigned int)process.start_time) {
+                process.state = READY;
+                ready_queue.push_back(process);
+                sync_queue(job_list, process);
+                execution_status += print_exec_status(current_time, process.PID, WAITING, READY);
+            } else {
+                still_waiting.push_back(process);
+            }
+        }
+        wait_queue = still_waiting;
+
+        
+        if(running.state == RUNNING && running.remaining_time == 0) {
+            execution_status += print_exec_status(current_time, running.PID, RUNNING, TERMINATED);
+            terminate_process(running, job_list);
+            idle_CPU(running);
+            last_io_at_cpu_time = 0;
+        }
+       
+        else if(running.state == RUNNING && running.io_freq > 0) {
+            unsigned int cpu_time_used = running.processing_time - running.remaining_time;
+            if(cpu_time_used > 0 && cpu_time_used % running.io_freq == 0 && 
+            last_io_at_cpu_time != cpu_time_used) {
+                execution_status += print_exec_status(current_time, running.PID, RUNNING, WAITING);
+                running.state = WAITING;
+                running.start_time = current_time + running.io_duration;
+                wait_queue.push_back(running);
+                sync_queue(job_list, running);
+                last_io_at_cpu_time = cpu_time_used;
+                idle_CPU(running);
+            }
+        }
+        /////////////////////////////////////////////////////////////////
+
+        //////////////////////////SCHEDULER//////////////////////////////
+        
+        std::sort(ready_queue.begin(), ready_queue.end(),
+            [](const PCB &first, const PCB &second) {
+                return first.PID > second.PID;
+            });
+
+        
+        if(running.state == NOT_ASSIGNED && !ready_queue.empty()) {
+            run_process(running, job_list, ready_queue, current_time);
+            execution_status += print_exec_status(current_time, running.PID, READY, RUNNING);
+        }
+
+        
+        if(running.state == RUNNING && running.remaining_time > 0) {
+            running.remaining_time--;
+            sync_queue(job_list, running);
+        }
+
+        current_time++;
+
+        if(ready_queue.empty() && wait_queue.empty() && running.state == NOT_ASSIGNED && all_process_terminated(job_list)) {
+            break;
+        }
+        /////////////////////////////////////////////////////////////////
+
+    }
+    
+    //Close the output table
+    execution_status += print_exec_footer();
+
+    return std::make_tuple(execution_status);
+}
+
+
+int main(int argc, char** argv) {
+
+    //Get the input file from the user
+    if(argc != 2) {
+        std::cout << "ERROR!\nExpected 1 argument, received " << argc - 1 << std::endl;
+        std::cout << "To run the program, do: ./interrutps <your_input_file.txt>" << std::endl;
+        return -1;
+    }
+
+    //Open the input file
+    auto file_name = argv[1];
+    std::ifstream input_file;
+    input_file.open(file_name);
+
+    //Ensure that the file actually opens
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Unable to open file: " << file_name << std::endl;
+        return -1;
+    }
+
+    //Parse the entire input file and populate a vector of PCBs.
+    //To do so, the add_process() helper function is used (see include file).
+    std::string line;
+    std::vector<PCB> list_process;
+    while(std::getline(input_file, line)) {
+        auto input_tokens = split_delim(line, ", ");
+        auto new_process = add_process(input_tokens);
+        list_process.push_back(new_process);
+    }
+    input_file.close();
+
+    //With the list of processes, run the simulation
+    auto [exec] = run_simulation(list_process);
+
+    write_output(exec, "execution.txt");
+
+    return 0;
+}
